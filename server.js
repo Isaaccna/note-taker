13dc0067107c177767b836ad9ce72cef14b64c7f
Isaaccna <isaac.andrade1231@gmail.com>
2022-01-04 15:43:49 -0500

    server updated

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/api/notes", (req,res) => { res.json(notesDB)});
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
 
 app.post("/api/notes", (req, res) => {
-  req.body.id = uuidv4();
+
   const newNote = req.body;
 
   notesDB.push(newNote);
