23cc84ed455ed9b4b5b7ec0bff1d7414c9b2e7f3
Isaaccna <isaac.andrade1231@gmail.com>
2022-01-05 18:00:16 -0500

    readme updated

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 //modules
 const express = require("express");
 const PORT = process.env.PORT || 3000 ;
-
 const path = require("path");
 const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 var notesDB = require("./db/db.json");
 
 const app = express();
@@ -20,7 +20,7 @@ app.get("/api/notes", (req,res) => { res.json(notesDB)});
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
 
 app.post("/api/notes", (req, res) => {
-
+  req.body.id = uuidv4();
   const newNote = req.body;
 
   notesDB.push(newNote);
@@ -29,10 +29,10 @@ app.post("/api/notes", (req, res) => {
   res.json(notesDB);
 })
 
-app.delete("/api/notes/:title", (req, res) => {
-  const id = req.params.title;
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
 
-  notesDB = notesDB.filter(notes => notes.title != title);
+  notesDB = notesDB.filter(notes => notes.id != id);
 
   fs.writeFileSync("./db/db.json", JSON.stringify(notesDB));
   res.json(notesDB);
